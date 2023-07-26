a17b6d80a471a99b47c886e0002c9fc18035e4b7
soumen-s <soumen1310.official@gmail.com>
2023-07-26 21:17:20 +0530

    rhombus with stars

---
+++ c pattern rhombus_with_star.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>                    //           *  *  *  *
+int main()                            //        *  *  *  *         for n=4
+{                                     //     *  *  *  *      
+   int n;                             //  *  *  *  *
+   printf ("Enter a number: ");           
+   scanf ("%d", &n);      
+   int k;                     
+   
+   for (int i=1; i<=n; i++)
+   {
+   	  for (int j=1; j<=(n-i); j++)
+   	  {
+   	  	printf ("  ");
+      }
+      for (int k=1; k<=n; k++)
+      {
+      	 printf ("* ");
+	  }
+	 printf ("\n"); 
+   }
+    
+ return 0;  
+}
