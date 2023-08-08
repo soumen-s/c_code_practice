740e83d392bcd654311e86b3271e5fbcfa48cd71
soumen-s <soumen1310.official@gmail.com>
2023-08-08 23:21:05 +0530

    interchange of 2 values of matrix

---
+++ c combi_of_3_nums.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+   for (int i=1; i<=3; i++)
+   {
+   	    for (int j=1; j<=3; j++)
+   	    {
+   	    	for (int k=1; k<=3; k++)
+   	    	{
+   	    		if(i!=j && i!=k && j!=k)
+   	    		{
+   	    		    printf ("%d%d%d\n",i,j,k);
+   	    	    }
+			}
+		}
+   }
+   
+   return 0;
+}
