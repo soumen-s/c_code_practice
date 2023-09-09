e6de5ab529075cbc17fa20c72548812d97f80db5
soumen-s <soumen1310.official@gmail.com>
2023-09-09 23:48:30 +0530

    program to create a sound

---
+++ c bell_ring.cpp
@@ -0,0 +1,11 @@
+// Program to print a character that produce a sound
+
+#include <stdio.h>
+
+int main() 
+{
+        
+    printf("\a");  // '/a' is a special character that produces sound
+    
+    return 0;
+}
