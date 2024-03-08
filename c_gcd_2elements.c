94deb22a3be1c87cae181db81f9ca2cddfa62983
Soumen Saha <soumen1310.official@gmail.com>
2024-03-08 22:53:04 +0530

    program to perform gcd of 2 numbers

---
+++ c_gcd_2elements.c
@@ -0,0 +1,31 @@
+// Program to find GCD of two numbers 
+
+#include <stdio.h> 
+  
+int gcd(int a, int b) 
+{ 
+    int result = ((a < b) ? a : b); 
+    
+    while (result > 0) 
+    { 
+        if (a%result == 0 && b%result == 0) // Check if both a and b are divisible by result 
+        { 
+            break; 
+        } 
+        result--; 
+    } 
+  return result; 
+} 
+  
+int main() 
+{ 
+    int a,b; 
+    
+    printf("Enter the first number: ");
+    scanf("%d", &a);
+    printf("Enter the second number: ");
+    scanf("%d", &b);
+    
+    printf("\nGCD of %d and %d is %d ", a, b, gcd(a, b)); 
+    return 0; 
+} 
